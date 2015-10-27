15ad60830b3ee3c2170d71fac5a95abd5f76ef62
Slava Fomin <s.fomin@betsol.ru>
2015-10-27 18:26:51 +0300

    Release 0.0.2.

--- changelog.md
+++ changelog.md
@@ -1,5 +1,12 @@
 # betsol-ng-repository changelog
 
+## Version 0.0.2
+(27 Oct 2015)
+
+- Extended entity modelization support
+- Added default modelization
+
+
 ## Version 0.0.1
 (25 Oct 2015)
 
--- src/module.js
+++ src/module.js
@@ -7,7 +7,7 @@
       return function Repository (url, options) {
 
         var defaultOptions = {
-          modelize: function () {},
+          modelize: false,
           responseTraverser: function (response) {
             return response;
           }
@@ -17,66 +17,145 @@
         // Normalizing specified URL.
         url = normalizeUrl(url);
 
-        return {
-          find: function (query, count, offset, sortParams) {
-            query = query || {};
-            if (count > 0) {
-              query.count = count;
-            }
-            if (offset >= 0) {
-              query.offset = offset;
-            }
-            if (sortParams) {
-              query.sort = querifySortParams(sortParams);
-            }
-            return request
-              .get(url, query)
-              .then(listEntitiesModelizer)
-            ;
-          },
-          findOneById: function (id, query) {
-            return request
-              .get(urlWithId(url, id), query)
-              .then(singleEntityModelizer)
-            ;
-          },
-          create: function (entity) {
-            return request
-              .post(url, null, entity)
-              .then(singleEntityModelizer)
-            ;
-          },
-          update: function (id, entity) {
-            return request
-              .put(urlWithId(url, id), null, entity)
-              .then(singleEntityModelizer)
-            ;
-          },
-          save: function (entity) {
-            if (entity.id) {
-              var id = entity.id;
-              delete entity.id;
-              return this.update(id, entity);
-            } else {
-              return this.create(entity);
-            }
-          },
-          'delete': function (id) {
-            return request.delete(urlWithId(url, id));
+        var instance = {};
+
+        instance.find = function (query, count, offset, sortParams) {
+          query = query || {};
+          if (count > 0) {
+            query.count = count;
+          }
+          if (offset >= 0) {
+            query.offset = offset;
+          }
+          if (sortParams) {
+            query.sort = querifySortParams(sortParams);
+          }
+          return request
+            .get(url, query)
+            .then(listEntitiesResponseModelizer)
+          ;
+        };
+
+        instance.findOneById = function (id, query) {
+          return request
+            .get(urlWithId(url, id), query)
+            .then(singleEntityResponseModelizer)
+          ;
+        };
+
+        instance.create = function (entity) {
+          return request
+            .post(url, null, entity)
+            .then(singleEntityResponseModelizer)
+          ;
+        };
+
+        instance.update = function (id, entity) {
+          return request
+            .put(urlWithId(url, id), null, getEntityData(entity))
+            .then(singleEntityResponseModelizer)
+          ;
+        };
+
+        instance.save = function (entity) {
+          if (entity.id) {
+            var id = entity.id;
+            delete entity.id;
+            return this.update(id, entity);
+          } else {
+            return this.create(entity);
           }
         };
 
+        instance.delete = function (id) {
+          return request.delete(urlWithId(url, id));
+        };
 
-        function singleEntityModelizer (response) {
+        return instance;
+
+
+        function getEntityData (entity) {
+          var data = {};
+          angular.forEach(entity, function (value, key) {
+            if ('function' === typeof value) {
+              return;
+            }
+            if ('$' == key.charAt(0)) {
+              return;
+            }
+            data[key] = value;
+          });
+          return data;
+        }
+
+        function modelizeEntity (entity) {
+          if (options.modelize) {
+            defaultModelizer(entity);
+            if ('function' === typeof options.modelize) {
+              options.modelize(entity);
+            }
+          }
+        }
+
+        function defaultModelizer (entity) {
+
+
+          // Maintaining copy of original entity in a sub-property for comparison.
+          entity.$master = angular.copy(entity);
+
+          /**
+           * Returns true, if entity was altered.
+           *
+           * @returns {boolean}
+           */
+          entity.isChanged = function () {
+            return !angular.equals(entity, entity.$master);
+          };
+
+          /**
+           * Returns subset of entity properties, that were changed and it's ID.
+           *
+           * @returns {Object}
+           */
+          entity.getChangedData = function () {
+            var changedData = {};
+            var data = getEntityData(this);
+            angular.forEach(this.$master, function (masterValue, property) {
+              var value = data[property];
+              if (!angular.equals(masterValue, value)) {
+                changedData[property] = value;
+              }
+            });
+            return changedData;
+          };
+
+          /**
+           * Saves entity if required.
+           */
+          entity.save = function () {
+            if (this.isChanged()) {
+              var data = this.getChangedData(entity);
+              if (entity.id) {
+                data.id = entity.id;
+              }
+              return instance.save(data);
+            } else {
+              return $q.resolve();
+            }
+          };
+
+        }
+
+        function singleEntityResponseModelizer (response) {
           var entity = options.responseTraverser(response);
-          options.modelize(entity);
+          modelizeEntity(entity);
           return response;
         }
 
-        function listEntitiesModelizer (response) {
+        function listEntitiesResponseModelizer (response) {
           var entities = options.responseTraverser(response);
           angular.forEach(entities, function (entity) {
-            options.modelize(entity);
+            modelizeEntity(entity);
           });
           return response;
         }
